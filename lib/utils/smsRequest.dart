a895744f3673c02a16ecf06916847c08fde3be35
Jan Rothen <jan.rothen@me.com>
2023-08-26 21:54:26 +0200

    removed prints

--- lib/utils/smsRequest.dart
+++ lib/utils/smsRequest.dart
@@ -3,17 +3,16 @@ import 'package:http/http.dart' as http;
 
 class SmsRequest {
   static Future<void> send(String phoneNumber) async {
-    final url = Uri.parse('https://xyycvixsvh.execute-api.eu-central-1.amazonaws.com/Prod/sms'); // Replace with your API endpoint
+    final url = Uri.parse(
+        'https://xyycvixsvh.execute-api.eu-central-1.amazonaws.com/Prod/sms');
     final headers = {'Content-Type': 'application/json'};
     final body = json.encode({"phoneNumber": phoneNumber});
 
     final response = await http.post(url, headers: headers, body: body);
 
     if (response.statusCode == 200) {
-      print('POST request successful');
       print('Response: ${response.body}');
     } else {
-      print('POST request failed');
       print('Status code: ${response.statusCode}');
       print('Response: ${response.body}');
     }
