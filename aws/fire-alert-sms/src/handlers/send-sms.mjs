ccc826d0f689dd00ed32191b2b3ef2554fc80fdd
Pascal Grüter <pascal.grueter@protonmail.ch>
2023-08-26 17:24:35 +0200

    Send SMS with AWS SNS Client

---
+++ aws/fire-alert-sms/src/handlers/send-sms.mjs
@@ -0,0 +1,31 @@
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns"; // ES Modules import
+
+export const sendSmsHandler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+
+    console.info('Received: ', event);
+
+    // Get phone number from the body of the request
+    const body = JSON.parse(event.body);
+    const phoneNumber = body.phoneNumber;
+    
+    const client = new SNSClient({});
+    const input = { 
+    Message: 'FireAlert: Smoke Detector Signal registered!',
+    PhoneNumber: phoneNumber,
+    Subject: 'FireAlert'};
+
+    const command = new PublishCommand(input);
+    const response = await client.send(command);
+
+    console.info('Response from SNS: ', response);
+
+    response = {
+        statusCode: response?.$metadata?.httpStatusCode
+    };
+
+    return response;
+};
+
